167a4159a31d59313bf0f564476f7dd11137e85b
Paulo Ricardo <notadead5@gmail.com>
2023-01-18 22:02:52 -0300

    Header, Summary and generate dates.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,16 @@
-import Habit from "./components/Habit"
+import {HabitDay} from "./components/HabitDay"
+import { Header } from "./components/Header"
+import { SummaryTable } from "./components/SummaryTable"
 import './components/styles/global.css'
 
-function App() {
+export function App() {
 
   return (
-    <div>
-      <Habit completed={3}></Habit>
-      <Habit completed={4}></Habit>
-      <Habit completed={5}></Habit>
-      <Habit completed={10}></Habit>
-      <Habit completed={13}></Habit>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header/>
+        <SummaryTable/>
+      </div>
     </div>
   )
 }
-
-export default App
---
+++ web/src/components/HabitDay.tsx
@@ -0,0 +1,11 @@
+interface HabitDayProps {
+    completed: number
+}
+
+export function HabitDay(props: HabitDayProps){
+    return(
+        <div className="w-10 h1- bg-zinc-900 border border-zinc-800 rounded-lg">
+            
+        </div>
+    )
+}

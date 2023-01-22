88eccaca239a635b6c6debfb2883e8a8cde84c08
Paulo Ricardo <notadead5@gmail.com>
2023-01-22 03:24:56 -0300

    Rotas de dias

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -110,6 +110,18 @@ async function run() {
         }
       }
     }),
+    prisma.day.create({
+      data: {
+        /** Wednesday */
+        date: new Date('2023-01-24T03:00:00.000z'),
+        dayHabits: {
+          create: [
+            { habit_id: firstHabitId },
+            { habit_id: secondHabitId },
+          ]
+        }
+      }
+    }),
   ])
 }
 

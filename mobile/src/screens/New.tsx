6cfb1e242fea93dfc1633f5f9b07474d6ba58afd
Paulo Ricardo <notadead5@gmail.com>
2023-01-26 23:58:54 -0300

    Components

---
+++ mobile/src/screens/New.tsx
@@ -0,0 +1,28 @@
+import { ScrollView, Text, TextInput, View } from "react-native";
+import { BackButton } from "../components/BackButton";
+import { Checkbox } from "../components/Checkbox";
+
+const availableWeekDays = ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado']
+
+export function New(){
+    return(
+        <View className="flex-1 bg-background px-8 pt-16">
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <BackButton/>
+                <Text className="mt-6 text-white font-extrabold text-3xl">
+                    Criar Hábito
+                </Text>
+                <Text className="mt-6 text-white font-semibold text-base">
+                    Qual o seu comprometimento?
+                </Text>
+                <TextInput
+                    className="h-12 pl-4 rounded-lg mt-3 bg-zinc-800 text-white focus:border-2 focus:border-green-600"
+
+                />
+                <Checkbox
+                    title="Beber 2L de água"
+                />
+            </ScrollView>
+        </View>
+    )
+}
